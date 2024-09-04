04d2f084cc7d761ecf7b31e4987b32e6f5b874db
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-04 13:57:17 +0700

    baru kesana

---
+++ 1-intro/Latihan19.java
@@ -0,0 +1,10 @@
+/** 
+ * Latihan19
+*/
+public class Latihan19 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama saya Muhammad Dwi Febrian");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi");
+    }
+}
---
+++ 1-intro/MyFirstJava19.java
@@ -0,0 +1,15 @@
+public class MyFirstJava19{
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Nama\t:Muhammad Dwi Febrian");
+        System.out.println("NIM\t:244107020081");
+        System.out.println("Kelas\t:1A");
+        System.out.println("No Absen:19");
+        System.out.println("Prodi\t:D4 Teknik Informatika");
+        System.out.println("Alamat\t:Besuki, Situbondo");
+        System.out.println("No HP\t:083134767575");
+        System.out.println("Hobi\t:Baca komik");
+    }
+}

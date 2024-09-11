97c25044760e57db2c5164f4c98f3355e57f8757
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-11 13:22:19 +0700

    perubahan

--- smt1/jobsheet3/contohVariabel.java
+++ smt1/jobsheet3/contohVariabel.java
@@ -1,16 +1,16 @@
 public class contohVariabel {
     
     public static void main(String[] args){
-        String salahSatuHobbySayaAdalah="bermain basket";
+        String hobby="bermain basket";
         char jenisKelamin='L';
         boolean isPandai=true;
-        byte _umurSayaSekarang=20;
+        byte umur=20;
         double $ipk=3.24, tinggi=1.78;
 
-        System.out.println(salahSatuHobbySayaAdalah);
+        System.out.println(hobby);
         System.out.println("apakah saya pandai?" + isPandai);
         System.out.println("jenis kelamin" + jenisKelamin);
-        System.out.println("umurku saat ini" + _umurSayaSekarang);
+        System.out.println("umurku saat ini" + umur);
         System.out.println(String.format("saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
 
     }

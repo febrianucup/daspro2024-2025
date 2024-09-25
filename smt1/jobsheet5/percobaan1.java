3e2ae9700a0bc69606898bf931d5f5ff481d5395
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-25 14:51:22 +0700

    ternary

--- smt1/jobsheet5/percobaan1.java
+++ smt1/jobsheet5/percobaan1.java
@@ -5,12 +5,16 @@ public class percobaan1 {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 
+        System.out.print("Masukkan Sebuah Angka: ");
         int angka=input.nextInt();
 
         if(angka%2==0){
-            System.out.println("Angka " + angka + " adalah ganjil");
-        }else{
             System.out.println("Angka " + angka + " adalah genap");
+        }else{
+            System.out.println("Angka " + angka + " adalah ganjil");
         }
+
+        String num=(angka%2==0) ? "Genap":"Ganjil";
+        System.out.println(num);
     }
 }

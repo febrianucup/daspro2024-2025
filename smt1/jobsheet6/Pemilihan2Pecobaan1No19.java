8ddd3f2fef80ee2150353c0325d75df06bef6e81
febrianucup <muhammaddwifebrian@gmail.com>
2024-10-02 05:47:34 +0700

    git

--- smt1/jobsheet6/Pemilihan2Pecobaan1No19.java
+++ smt1/jobsheet6/Pemilihan2Pecobaan1No19.java
@@ -17,6 +17,10 @@ public class Pemilihan2Pecobaan1No19 {
         if(tahun%4==0){
             if(tahun%100!=0){
                 System.out.println("Tahun Kabisat");
+            }else if(tahun%100==0){
+                if(tahun%400==0){
+                    System.out.println("Tahun Kabisat");
+                }
             }else{
                 System.out.println("Bukan Tahun Kabisat");
             }

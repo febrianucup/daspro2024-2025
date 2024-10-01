e1932959f4f828b4117004c07fe584a8c987c167
febrianucup <muhammaddwifebrian@gmail.com>
2024-10-01 16:24:11 +0700

    perubahan

--- smt1/jobsheet6/Pemilihan2Percobaan2No19.java
+++ smt1/jobsheet6/Pemilihan2Percobaan2No19.java
@@ -8,8 +8,8 @@ public class Pemilihan2Percobaan2No19 {
         Scanner input=new Scanner(System.in);
         
         //deklarasi variabel
-        String menu, member;
-        int jumlah_beli;
+        String menu, member, qris;
+        int jumlah_beli, potongan=1000;
         double total_bayar=0, diskon, harga=0;
 
         //input
@@ -22,35 +22,69 @@ public class Pemilihan2Percobaan2No19 {
         menu=input.nextLine().toLowerCase();
         System.out.print("Masukkan Jumlah: ");
         jumlah_beli=input.nextInt();
+        input.nextLine();
+        System.out.print("Apakah menggunakan Qris (y/t) ? ");
+        qris=input.nextLine().toLowerCase();
 
         //controlstatement
-        if(member.equals("y")){
-            diskon=0.1;
-            if(menu.equals("kopi")){
-                harga=12000;
-                System.out.println("Harga Kopi: " + harga);
-            }else if(menu.equals("teh")){
-                harga=7000;
-                System.out.println("Harga Teh: " + harga);
-            }else if(menu.equals("roti")){
-                harga=20000;
-                System.out.println("Harga Roti: " + harga);
+        if(qris.equals("y")){
+            if(member.equals("y")){
+                diskon=0.1;
+                if(menu.equals("kopi")){
+                    harga=12000;
+                    System.out.println("Harga Kopi: " + harga);
+                }else if(menu.equals("teh")){
+                    harga=7000;
+                    System.out.println("Harga Teh: " + harga);
+                }else if(menu.equals("roti")){
+                    harga=20000;
+                    System.out.println("Harga Roti: " + harga);
+                }
+    
+                //menghitung total bayar dengan diskon
+                total_bayar=harga-(harga*diskon)*jumlah_beli-potongan;
+            }else{
+                if(menu.equals("kopi")){
+                    harga=12000;
+                    System.out.println("Harga Kopi: " + harga);
+                }else if(menu.equals("teh")){
+                    harga=7000;
+                    System.out.println("Harga Teh: " + harga);
+                }else if(menu.equals("roti")){
+                    harga=20000;
+                    System.out.println("Harga Roti: " + harga);
+                }
+                total_bayar=harga*jumlah_beli-potongan;
             }
-
-            //menghitung total bayar dengan diskon
-            total_bayar=harga-(harga*diskon)*jumlah_beli;
         }else{
-            if(menu.equals("kopi")){
-                harga=12000;
-                System.out.println("Harga Kopi: " + harga);
-            }else if(menu.equals("teh")){
-                harga=7000;
-                System.out.println("Harga Teh: " + harga);
-            }else if(menu.equals("roti")){
-                harga=20000;
-                System.out.println("Harga Roti: " + harga);
+            if(member.equals("y")){
+                diskon=0.1;
+                if(menu.equals("kopi")){
+                    harga=12000;
+                    System.out.println("Harga Kopi: " + harga);
+                }else if(menu.equals("teh")){
+                    harga=7000;
+                    System.out.println("Harga Teh: " + harga);
+                }else if(menu.equals("roti")){
+                    harga=20000;
+                    System.out.println("Harga Roti: " + harga);
+                }
+    
+                //menghitung total bayar dengan diskon
+                total_bayar=harga-(harga*diskon)*jumlah_beli;
+            }else{
+                if(menu.equals("kopi")){
+                    harga=12000;
+                    System.out.println("Harga Kopi: " + harga);
+                }else if(menu.equals("teh")){
+                    harga=7000;
+                    System.out.println("Harga Teh: " + harga);
+                }else if(menu.equals("roti")){
+                    harga=20000;
+                    System.out.println("Harga Roti: " + harga);
+                }
+                total_bayar=harga*jumlah_beli;
             }
-            total_bayar=harga*jumlah_beli;
         }
         //output
         System.err.println("============================");

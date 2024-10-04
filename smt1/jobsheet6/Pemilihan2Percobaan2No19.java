f0013667c5ac31df14deb551520197a728fcb195
febrianucup <muhammaddwifebrian@gmail.com>
2024-10-04 21:42:54 +0700

    p

--- smt1/jobsheet6/Pemilihan2Percobaan2No19.java
+++ smt1/jobsheet6/Pemilihan2Percobaan2No19.java
@@ -8,88 +8,93 @@ public class Pemilihan2Percobaan2No19 {
         Scanner input=new Scanner(System.in);
         
         //deklarasi variabel
-        String menu, member, qris;
-        int jumlah_beli, potongan=1000;
+        String member, qris;
+        int jumlah_beli, potongan=1000, menu;
         double total_bayar=0, diskon, harga=0;
 
         //input
         System.err.println("============================");
         System.out.println("\tMenu Cafe 1A");
         System.err.println("============================");
-        System.out.print("Punya Member (y/t) ? ");
-        member=input.nextLine().toLowerCase();
+        System.out.println("1. Rice Bowl");
+        System.out.println("2. Ice Tea");
+        System.out.println("3. Paket Bunding (Rice Bowl + Ice Tea)");
+        System.out.println("============================");
         System.out.print("Masukkan Menu: ");
-        menu=input.nextLine().toLowerCase();
+        menu=input.nextInt();
         System.out.print("Masukkan Jumlah: ");
         jumlah_beli=input.nextInt();
         input.nextLine();
+        System.out.print("Punya Member (y/t) ? ");
+        member=input.nextLine();
         System.out.print("Apakah menggunakan Qris (y/t) ? ");
-        qris=input.nextLine().toLowerCase();
+        qris=input.nextLine();
 
         //controlstatement
-        if(qris.equals("y")){
-            if(member.equals("y")){
-                diskon=0.1;
-                if(menu.equals("kopi")){
-                    harga=12000;
-                    System.out.println("Harga Kopi: " + harga);
-                }else if(menu.equals("teh")){
-                    harga=7000;
-                    System.out.println("Harga Teh: " + harga);
-                }else if(menu.equals("roti")){
-                    harga=20000;
-                    System.out.println("Harga Roti: " + harga);
+        if(member.equalsIgnoreCase("y")){
+            diskon=0.1;
+            if(qris.equalsIgnoreCase("y")){
+                if(menu==1){
+                    harga=14000;
+                    System.out.println("Harga RIce Bowl: " + harga);
+                }else if(menu==2){
+                    harga=3000;
+                    System.out.println("Harga Ice Tea: " + harga);
+                }else if(menu==3){
+                    harga=15000;
+                    System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
                 }
     
                 //menghitung total bayar dengan diskon
                 total_bayar=harga-(harga*diskon)*jumlah_beli-potongan;
-            }else{
-                if(menu.equals("kopi")){
-                    harga=12000;
-                    System.out.println("Harga Kopi: " + harga);
-                }else if(menu.equals("teh")){
-                    harga=7000;
-                    System.out.println("Harga Teh: " + harga);
-                }else if(menu.equals("roti")){
-                    harga=20000;
-                    System.out.println("Harga Roti: " + harga);
+                System.out.println("Total Bayar Sebesar " + total_bayar);
+            }else if(qris.equalsIgnoreCase("t")){
+                if(menu==1){
+                    harga=14000;
+                    System.out.println("Harga RIce Bowl: " + harga);
+                }else if(menu==2){
+                    harga=3000;
+                    System.out.println("Harga Ice Tea: " + harga);
+                }else if(menu==3){
+                    harga=15000;
+                    System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
                 }
                 total_bayar=harga*jumlah_beli-potongan;
-            }
-        }else{
-            if(member.equals("y")){
-                diskon=0.1;
-                if(menu.equals("kopi")){
-                    harga=12000;
-                    System.out.println("Harga Kopi: " + harga);
-                }else if(menu.equals("teh")){
-                    harga=7000;
-                    System.out.println("Harga Teh: " + harga);
-                }else if(menu.equals("roti")){
-                    harga=20000;
-                    System.out.println("Harga Roti: " + harga);
-                }
-    
-                //menghitung total bayar dengan diskon
-                total_bayar=harga-(harga*diskon)*jumlah_beli;
+                System.out.println("Total Bayar Sebesar " + total_bayar);
             }else{
-                if(menu.equals("kopi")){
-                    harga=12000;
-                    System.out.println("Harga Kopi: " + harga);
-                }else if(menu.equals("teh")){
-                    harga=7000;
-                    System.out.println("Harga Teh: " + harga);
-                }else if(menu.equals("roti")){
-                    harga=20000;
-                    System.out.println("Harga Roti: " + harga);
+                System.out.println("Inputan Qris Salah");
+            }
+        }else if(member.equalsIgnoreCase("y")){
+            diskon=0.1;
+            if(menu==1){
+                harga=14000;
+                System.out.println("Harga RIce Bowl: " + harga);
+            }else if(menu==2){
+                harga=3000;
+                System.out.println("Harga Ice Tea: " + harga);
+            }else if(menu==3){
+                harga=15000;
+                System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
+            }
+
+            //menghitung total bayar dengan diskon
+            total_bayar=harga-(harga*diskon)*jumlah_beli;
+            System.out.println("Total Bayar Sebesar " + total_bayar);
+        }else if(member.equalsIgnoreCase("t")){
+                if(menu==1){
+                    harga=14000;
+                    System.out.println("Harga RIce Bowl: " + harga);
+                }else if(menu==2){
+                    harga=3000;
+                    System.out.println("Harga Ice Tea: " + harga);
+                }else if(menu==3){
+                    harga=15000;
+                    System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
                 }
                 total_bayar=harga*jumlah_beli;
-            }
+                System.out.println("Total Bayar Sebesar " + total_bayar);
+        }else{
+            System.out.println("Inputan Member Salah");
         }
-        //output
-        System.err.println("============================");
-        System.out.println("\tTotal Bayar");
-        System.err.println("============================");
-        System.out.println("Total Bayar Sebesar "+total_bayar);
     }
 }
--- smt1/jobsheet6/TokoSepatu.java
+++ smt1/jobsheet6/TokoSepatu.java
@@ -8,29 +8,39 @@ public class TokoSepatu {
         Scanner input=new Scanner(System.in);
 
         //deklarasi variabel
-        String merk, kategori;
-        int ukuran, harga=0;
+        int kategori;
+        int merk, ukuran, harga=0;
 
         //input
         System.err.println("============================");
         System.out.println("       Toko Sepatu 1A");
         System.err.println("============================");
-        System.out.print("Masukkan Merk= ");
-        merk=input.nextLine().toLowerCase();
-        System.out.print("Masukkan Kategori= ");
-        kategori=input.nextLine().toLowerCase();
+        System.out.println("1. Converse:");
+        System.out.println("\t 1. Slip On");
+        System.out.println("\t 2. High Top");
+        System.out.println("2. Sketcher:");
+        System.out.println("\t 1. Woman");
+        System.out.println("\t 2. Man");
+        System.out.println("3. Nike:");
+        System.out.println("\t 1. Kids");
+        System.out.println("\t 2. Adult");
+        System.out.print("Pilih Merk= ");
+        merk=input.nextInt();
+        input.nextLine();
+        System.out.print("Pilih Kategori= ");
+        kategori=input.nextInt();
         System.out.print("Masukkan Ukuran Sepatu= ");
         ukuran=input.nextInt();
 
         //pemilihan
-        if(merk.equals("converse")){
-            if(kategori.equals("slip on")){
+        if(merk==1){
+            if(kategori==1){
                 if(ukuran>=36 && ukuran<=40){
                     harga=800000;
                 }else{
                     System.out.println("invalid");
                 }
-            }else if(kategori.equals("high top")){
+            }else if(kategori==2){
                 if(ukuran>=40 && ukuran<=44){
                     harga=1200000;
                 }else{
@@ -39,14 +49,14 @@ public class TokoSepatu {
             }else{
                 System.out.println("invalid kategori");
             }
-        }else if(merk.equals("sketcher")){
-            if(kategori.equals("woman")){
+        }else if(merk==2){
+            if(kategori==1){
                 if(ukuran>=36 && ukuran<=41){
                     harga=1000000;
                 }else{
                     System.out.println("invalid");
                 }
-            }else if(kategori.equals("man")){
+            }else if(kategori==2){
                 if(ukuran>=41 && ukuran<=44){
                     harga=1800000;
                 }else{
@@ -55,14 +65,15 @@ public class TokoSepatu {
             }else{
                 System.out.println("invalid kategori");
             }
-        }else if(merk.equals("nike")){
-            if(kategori.equals("kids")){
+        }else if(merk==3){
+            if(kategori==1){
                 if(ukuran>=36 && ukuran<=40){
                     harga=750000;
                 }else{
                     System.out.println("invalid");
+
                 }
-            }else if(kategori.equals("adult")){
+            }else if(kategori==2){
                 if(ukuran>=40 && ukuran<=44){
                     harga=1500000;
                 }else{

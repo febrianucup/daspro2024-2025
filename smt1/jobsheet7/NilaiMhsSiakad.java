e66cc56de5076a6347e4bafcb0e55a5894deadaa
febrianucup <muhammaddwifebrian@gmail.com>
2024-10-09 13:19:46 +0700

    for

---
+++ smt1/jobsheet7/NilaiMhsSiakad.java
@@ -0,0 +1,26 @@
+package jobsheet7;
+
+import java.util.Scanner;
+
+public class NilaiMhsSiakad {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        //deklarasi variabel
+        double nilai, tertinggi=0, terendah=100;
+
+        //looping
+        for(int i=1;i<=10;i++){
+            System.out.print("Masukkan Nilai: ");
+            nilai=input.nextDouble();
+            if(nilai>tertinggi){
+                tertinggi=nilai;
+            }
+            if(nilai<terendah){
+                terendah=nilai;
+            }
+        }
+        System.out.println("Nilai Tertinggi= " + tertinggi);
+        System.out.println("Nilai Terendah= " + terendah);
+    }
+}

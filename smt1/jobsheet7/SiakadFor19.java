ccb28089d364df5c36a78583d158899867a4394a
febrianucup <muhammaddwifebrian@gmail.com>
2024-10-11 18:45:37 +0700

    hapus line

--- smt1/jobsheet7/SiakadFor19.java
+++ smt1/jobsheet7/SiakadFor19.java
@@ -7,7 +7,7 @@ public class SiakadFor19 {
         Scanner input=new Scanner(System.in);
 
         //deklarasi variabel
-        double nilai, tertinggi=0, terendah=100;
+        double nilai, tertinggi=0, terendah=100, lulus=0, tidakLulus=0;
 
         //looping
         for(int i=1;i<=10;i++){
@@ -19,8 +19,16 @@ public class SiakadFor19 {
             if(nilai<terendah){
                 terendah=nilai;
             }
+
+            if(nilai>=60){
+                lulus++;
+            }else if(nilai<60){
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai Tertinggi= " + tertinggi);
         System.out.println("Nilai Terendah= " + terendah);
+        System.out.println("Jumlah siswa yang lulus: " + lulus);
+        System.out.println("Jumlah siswa yang tidak lulus: " + tidakLulus);
     }
 }

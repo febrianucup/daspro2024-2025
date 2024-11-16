16ad5594830faf895067b2a9c534f279119b66c9
febrianucup <muhammaddwifebrian@gmail.com>
2024-11-16 10:10:26 +0700

    tambahan

--- smt1/Latihan/SkorBola.java
+++ smt1/Latihan/SkorBola.java
@@ -14,18 +14,17 @@ public class SkorBola {
         //array skor
         int[][] skor=new int[jmlhTim][jmlhTim];
         String[] namaTim=new String[jmlhTim];
+        int[] skorPerTim=new int[jmlhTim];
 
         //input nama tim
         for (int i = 0; i < namaTim.length; i++) {
             System.out.print("Nama tim " + (i+1) + ": ");
             namaTim[i]=input.nextLine();
         }
-
         System.out.println();
-
         //input skor pertandingan
         int inputSkor;
-        System.out.println("Masukkan skor pertangingan: ");
+        System.out.println("Masukkan skor pertandingan: ");
         for (int i = 0; i < skor.length; i++) {
             for (int j = 1; j < skor.length; j++) {
                 if (i==j) {
@@ -38,7 +37,7 @@ public class SkorBola {
                 skor[j][i]+=(-inputSkor);
             }       
         }
-
+        System.out.println();
         //tabel skor
         System.out.println("Tabel skor pertandingan: ");
         System.out.print("\t");
@@ -53,16 +52,26 @@ public class SkorBola {
             }
             System.out.println();
         }
-
         System.out.println();
-
         //total skor tiap tim
         for (int i = 0; i < skor.length; i++) {
             int totalSKor=0;
             for (int j = 0; j < skor[i].length; j++) {
                 totalSKor+=skor[i][j];
             }
-            System.out.println(namaTim[i] + " total skor " + totalSKor);
+            skorPerTim[i]+=totalSKor;
+            System.out.println(namaTim[i] + " total skor " + skorPerTim[i]);
         }
+        System.out.println();
+        //pemimpin klasemen
+        int idxTop=0;
+        int poinTertinggi=0;
+        for (int i = 0; i < skorPerTim.length; i++) {
+            if(skorPerTim[i]>poinTertinggi){
+                poinTertinggi=skorPerTim[i];
+                idxTop=i;
+            }
+        }
+        System.out.println("Pemimpin klasemen: " + namaTim[idxTop]);
     }
 }
